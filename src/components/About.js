88b55e3e24e17b7be75ff379115a1b9b81def3fd
reactInfoCard <momtazfarshad@gmail.com>
2023-08-10 18:40:21 +0330

    About

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,8 @@
 export default function About() {
-
+    return(
+        <div className='about'>
+            <h2>About</h2>
+            <p>i'm a software developer from iran and i study in tehran markaz branch in azad university and try to be a website developer</p>
+        </div>
+    )
 }
