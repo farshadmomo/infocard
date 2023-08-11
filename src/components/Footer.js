6e846ae0cd261974483271744fd04e1595051da0
reactInfoCard <momtazfarshad@gmail.com>
2023-08-11 19:37:29 +0330

    last touches 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,10 @@
 export default function Footer() {
-
+    return(
+        <div className='socials'>
+            <button><i className="gg-twitter"></i></button>
+            <button><i className="gg-facebook"></i></button>
+            <button><i className="gg-instagram"></i></button>
+            <button><i className="gg-git-branch"></i></button>
+        </div>
+    )
 }
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,3 +1,10 @@
-export default function Interests() {
+import profileimg from "../images/profile1.jpg";
 
+export default function Interests() {
+    return(
+        <div className='titleAbout'>
+            <h2>Interests</h2>
+            <p>I'm interested in coding and games plus animations i love to try three js sometime and 3d designing</p>
+        </div>
+    )
 }
